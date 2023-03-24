18c7e51a061addb6d9bb8c4ba1434480ffc2d749
Nourhan Zaki <nzaki@vilant.com>
2023-03-24 12:36:20 +0200

    adding ConstraintViolationException exception to exception handler

--- src/main/java/com/musala/task/drones/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/musala/task/drones/exceptions/RestResponseEntityExceptionHandler.java
@@ -1,19 +1,19 @@
 package com.musala.task.drones.exceptions;
 
-import com.musala.task.drones.exceptions.ExceedWeightException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({ ExceedWeightException.class , NotEnoughChargeException.class , NotFoundException.class })
-    public ResponseEntity<Object> handleExceedWeightException(
+    @ExceptionHandler({ ExceedWeightException.class , NotEnoughChargeException.class ,
+            NotFoundException.class , ConstraintViolationException.class})
+    public ResponseEntity<Object> handleExceptions(
             Exception ex) {
         return new ResponseEntity<> (
                 ex.getMessage (), new HttpHeaders (), HttpStatus.BAD_REQUEST);
